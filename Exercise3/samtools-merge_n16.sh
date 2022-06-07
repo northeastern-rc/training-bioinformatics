85d1b2e35e2255063dcb69f21abb33a607d1d7c4
ShobanaSekar <ssekar2@asu.edu>
2022-06-07 02:41:15 -0400

    2021 bootcamp material

---
+++ Exercise3/samtools-merge_n16.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#####Use after training##############
+##SBATCH -p short
+#####################################
+
+#####Use during training#############
+#SBATCH --partition=reservation
+#SBATCH --reservation=bootcamp2021cpu
+#####################################
+
+#####Use during &after training######
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task=16
+#SBATCH -t 1:00:00
+#SBATCH --mem=1G
+#SBATCH -o Samtools.%N.%j.out
+#SBATCH -e Samtools.%N.%j.err
+#SBATCH --job-name="Samtools-merge_test"
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=s.sekar@northeastern.edu
+
+
+# Load the system wide samtools module 
+module load samtools
+
+# Define the file with list of bams
+BAMLIST=bamlist.txt
+
+# Run Samtools with 16 CPUs specified using "@"
+echo "Running samtools with 16 cpus.."
+samtools merge merged_n16.bam -b ${BAMLIST} -@16
